5a0009fb9b36a117c58f23f50db4ceaab978e396
dacianG <dacian.goina00@e-uvt.ro>
2021-08-03 11:59:36 +0300

    default values, ids

--- src/app/components/schedule/schedule.component.ts
+++ src/app/components/schedule/schedule.component.ts
@@ -30,7 +30,7 @@ export class ScheduleComponent implements OnInit {
     this.id = this.route.snapshot.params['id'];
     this.classDTOService.getClassDTOById(this.id).subscribe(data =>{
       this.classDTOobj = data;
-      console.log(this.classDTOobj.courseName + " predat de" + this.classDTOobj.teacherFirstName + " " + this.classDTOobj.teacherLastName)
+      console.log(this.classDTOobj.courseName + " predat de" + this.classDTOobj.teacherFirstName + " " + this.classDTOobj.teacherLastName);
     },error => console.log(error));
 
     this.classDTOService.getClassroomsDetails().subscribe(data=>{
@@ -39,6 +39,7 @@ export class ScheduleComponent implements OnInit {
       for (let i of this.clDetails) {
         console.log(i.name + " | " + i.capacity + " locuri");
       }
+      console.log("Inside get clDetails: " + this.classDTOobj.studentsNumber);
     });
 
     this.classDTOService.getCourseNames().subscribe(data =>{
@@ -51,6 +52,32 @@ export class ScheduleComponent implements OnInit {
 
     this.giveNumToCap(8);
 
+  }
+
+  sendUpdates():void{
+    // get element by ID pentru cele 2 selectoare
+    let classroomName = (document.getElementById("selectClassroom")) as HTMLSelectElement;
+    let courseName = (document.getElementById("selectCourse")) as HTMLSelectElement;
+
+    // cu astea de dateTime nu am gasit inca cum sa le scot ca obiecte (vezi ca si mai sus se foloseste acel "as")
+    // si aici probabil e ceva de ceva de genul ca sa obtinem valorile lor
+    // dar oricum, poate folosim altceva inafara de date time local
+    let startTime = (document.getElementById("startTime"));
+    let endTime = document.getElementById("endTime");
+
+
+    // pentru a obtine valorile selectate (adica alea din varf)
+    let aux1 = classroomName.options[classroomName.selectedIndex];
+    let val1 = (<HTMLSelectElement><unknown>aux1).value;
+
+    let aux2 = courseName.options[courseName.selectedIndex];
+    let val2 = (<HTMLSelectElement><unknown>aux2).value;
+
+    // aici doar le afisez
+    console.log("Nume clasa: " + val1);
+    console.log("Nume curs: " + val2);
+    //console.log(startTime.innerText);
+    console.log("Planner id: " + this.classDTOobj.id);
   }
 
 
