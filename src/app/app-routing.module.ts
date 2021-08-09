9c322c638fcc7511b27c6ad60f7cb34f40c289d5
Beatrix <beatrix.racz00@e-uvt.ro>
2021-08-09 10:52:10 +0300

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
     canActivate: [CreatePageGuard],
   },
   {
-    path:'class/:id/enroll',
+    path:'class/:id/enroll/:stId',
     component: EnrollComponent,
   },
   {
